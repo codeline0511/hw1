79965e4d7016d2aa685f3ad5655ac2ca2d95e26a
codeline0511 <89986502+codeline0511@users.noreply.github.com>
2024-04-25 22:33:15 +0900

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -12,25 +12,25 @@ def read_number(a):
     return a_1
 
 def read_single_digit(n):
-    if n == '0':
+    if n == 0:
         return '영'
-    elif n == '1':
+    elif n == 1:
         return '일'
-    elif n == '2':
+    elif n == 2:
         return '이'
-    elif n == '3':
+    elif n == 3:
         return '삼'
-    elif n == '4':
+    elif n == 4:
         return '사'
-    elif n == '5':
+    elif n == 5:
         return '오'
-    elif n == '6':
+    elif n == 6:
         return '육'
-    elif n == '7':
+    elif n == 7:
         return '칠'
-    elif n == '8':
+    elif n == 8:
         return '팔'
-    elif n == '9':
+    else:
         return '구'
 
 num = input("세 자리 정수 입력: ")
