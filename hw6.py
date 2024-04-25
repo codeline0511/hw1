1dd95b01d49a3d920a22996e80ef4e4c6615c4b3
codeline0511 <89986502+codeline0511@users.noreply.github.com>
2024-04-25 14:25:11 +0900

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,14 +1,14 @@
 for i in range(1, 10):
-    print(f'2 x {i} = {2 * i}', end ='\t')
-    print(f'3 x {i} = {3 * i}', end ='\t')
-    print(f'4 x {i} = {4 * i}', end ='\t')
-    print(f'5 x {i} = {5 * i}', end ='\t')
+    print(f'2 x {i} = {2 * i:2d}', end ='\t')
+    print(f'3 x {i} = {3 * i:2d}', end ='\t')
+    print(f'4 x {i} = {4 * i:2d}', end ='\t')
+    print(f'5 x {i} = {5 * i:2d}', end ='\t')
     print()
 
 print()
 for i in range(1, 10):
-    print(f'6 x {i} = {6 * i}', end ='\t')
-    print(f'7 x {i} = {7 * i}', end ='\t')
-    print(f'8 x {i} = {8 * i}', end ='\t')
-    print(f'9 x {i} = {9 * i}', end ='\t')
+    print(f'6 x {i} = {6 * i:2d}', end ='\t')
+    print(f'7 x {i} = {7 * i:2d}', end ='\t')
+    print(f'8 x {i} = {8 * i:2d}', end ='\t')
+    print(f'9 x {i} = {9 * i:2d}', end ='\t')
     print()
